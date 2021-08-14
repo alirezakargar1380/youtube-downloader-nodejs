6279b9e4dc8d0db7c108234feab0ee2ad0b61150
alireza kargar <imanesche@gmail.com>
2021-08-14 14:36:35 +0430

    add download GET API

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello world")
+const express = require("express");
+const ytdl = require("ytdl-core");
+const app = express();
+app.use('/static', express.static('./static'));
+
+app.get('/download', (req, res) => {
+  var url = "https://www.youtube.com/watch?v=hS5CfP8n_js&ab_channel=Mr.Monk";
+  res.header("Content-Disposition", 'attachment;\  filename="Video.mp4');
+  ytdl(url, {format: 'mp4'}).pipe(res);
+});
+
+app.listen(3000, () => {
+  console.log("It Works!");
+});
